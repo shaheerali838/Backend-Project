1c4369d856348d2bffe1d19556dada96720e1dbb
shaheerali838 <shaheer838838@gmail.com>
2025-08-03 16:04:25 +0500

    Multer and cloudinary added

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import { log } from "console";
+import { response } from "express";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUDE_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    // yaha se file upload hoga
+    cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // yaha file upload hogaya h...
+    console.log(`File has been uploaded successfully ${response.url}`);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //ye locally save file jo k upload operation fail hpgaya h remove kardega
+    return null;
+  }
+};
